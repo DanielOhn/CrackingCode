24bab55c24893337ffa66d135ad0cb91e62edc8a
DanielOhn <catsby@Daniels-MacBook-Air.local>
2022-02-15 15:40:53 -0700

    Finish ZeroMatrix

--- ZeroMatrix.py
+++ ZeroMatrix.py
@@ -4,40 +4,32 @@ M x N matrix is 0, its entire row and col are set to 0.
 """
 def setZero(matrix):
     newMatrix = []
-    zero = False
+    zeros = {"rows": [], "cols": []}
+
+    # Get Location of all Zeros in the Matrix
+    for i in range(len(matrix)):
+        for j in range(len(matrix[i])):
+            if (matrix[i][j] == 0):
+                print("({0}, {1})".format(i, j))
+                zeros["rows"].append(i)
+                zeros["cols"].append(j)
+
+    # Iterates through matrix again and places Zeros in the correct place
     for i in range(len(matrix)):
         newMatrix.append([])
-        zero = False
-        for j in matrix[i]:
-            if j == 0:
+        for j in range(len(matrix[i])):
+            if (i in zeros["rows"] or j in zeros["cols"]):
                 newMatrix[i].append(0)
-                zero = True
-                if (i > 0):
-                    newMatrix[i-1].pop()
-                    # newMatrix[i-1].append(0)
-            elif j != 0 and zero:
-                newMatrix[i].append(0)
-            else: 
-                newMatrix[i].append(j)
+            else:
+                newMatrix[i].append(matrix[i][j])
 
     return newMatrix
-
-
-
-def matrix(m, n):
-    lst = []
-    for i in range(m):
-        lst.append([])
-        for j in n:
-            lst[i].append(j)
-    
-    return lst 
 
 def printM(m):
     for i in m:
         print(*i)
 
-mat = matrix(3, [1, 0, 3, 5])
+mat = [[1,2,3],[5,0,6],[7,8,9]]
 zero = setZero(mat)
 
 printM(mat)
