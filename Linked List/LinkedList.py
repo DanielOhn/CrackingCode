a14d852f54eb293092f6ce026307b570eb32666f
DanielOhn <catsby@Daniels-MacBook-Air.local>
2022-02-18 15:55:16 -0700

    Create LinkedList file and folder.

---
+++ Linked List/LinkedList.py
@@ -0,0 +1,42 @@
+class Node():
+    def __init__(self, next=None, data=None):
+        self.next = next
+        self.data = data
+
+    def __repre__(self):
+        return self.data
+
+class LinkedList():
+    def __init__(self):
+        self.head = None
+
+    def addNode(self, data):
+        if (self.head == None):
+            self.head = Node(data=data)
+        else:
+            node = self.head
+            while node is not None:
+                if (node.next == None):
+                    node.next = Node(data=data)
+                    break
+                else:
+                    node = node.next
+
+    def __repr__(self):
+        node = self.head
+        rslt = ""
+
+        while node is not None:
+            rslt += "{} > ".format(node.data)
+            node = node.next
+        
+        rslt += "None"
+        
+        return rslt
+
+linked_list = LinkedList()
+linked_list.addNode(5)
+linked_list.addNode(2)
+linked_list.addNode(4)
+
+print(repr(linked_list))
