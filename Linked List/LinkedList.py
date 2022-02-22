323bda0c0324224e1deb4c8cbb23643d1da5b48d
DanielOhn <ohndaniel@gmail.com>
2022-02-22 14:22:48 -0700

    Add removeDuplicates function in linked list.

--- Linked List/LinkedList.py
+++ Linked List/LinkedList.py
@@ -11,6 +11,7 @@ class LinkedList():
     def __init__(self):
         self.head = None
 
+    # Add a node to the end of the linked list
     def addNode(self, data):
         if (self.head == None):
             self.head = Node(data=data)
@@ -23,6 +24,7 @@ class LinkedList():
                 else:
                     node = node.next
     
+    # Remove the first node in the linked list that matches the value
     def removeNode(self, data):
         if (self.head == None):
             return "There are no nodes in this linked list"
@@ -35,6 +37,23 @@ class LinkedList():
                     node.next = temp.next
                     break
                 else:
+                    node = node.next
+    
+    # Remove all duplicates within the linked list
+    def removeDuplicates(self):
+        if (self.head == None):
+            return "No nodes in this linked list."
+        else: 
+            node = self.head
+            values = []
+            while node is not None:
+                temp = node.next
+
+                if (node.data not in values):
+                    values.append(node.data)
+                    node = node.next
+                else:
+                    self.removeNode(node.data)
                     node = node.next
 
     def __repr__(self):
@@ -53,7 +72,14 @@ linked_list = LinkedList()
 linked_list.addNode(5)
 linked_list.addNode(2)
 linked_list.addNode(4)
+linked_list.addNode(6)
+linked_list.addNode(2)
+linked_list.addNode(9)
+linked_list.addNode(2)
+linked_list.addNode(3)
+linked_list.addNode(1)
+print(repr(linked_list))
 
-linked_list.removeNode(4)
+linked_list.removeDuplicates()
 
 print(repr(linked_list))
