2adec28749b6d2b32975c719e953368d76e1dfbb
DanielOhn <ohndaniel@gmail.com>
2022-05-10 14:25:29 -0700

    Add Recursion folder and TripleStep.py.

---
+++ Recursion/TripleStep.py
@@ -0,0 +1,18 @@
+""" 
+A child is running up a staircase with n steps and can hop either 1 step, 2 step, or step at a time.
+Implement a method to count how many possible ways the child can run up the stairs.
+"""
+
+
+def TripleStep(n):
+    if (n == 0):
+        return 1
+    elif (n < 0):
+        return 0
+    else:
+        return TripleStep(n - 3) + TripleStep(n - 2) + TripleStep(n - 1)
+
+
+
+step = TripleStep(5)
+print(step)
